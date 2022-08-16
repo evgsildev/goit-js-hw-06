bee684ea6cd7646020d0c51462f0e45dc75cc7c6
Evgenii Silenko <evgen.silen@gmail.com>
2022-08-16 23:36:51 +0300

    9/10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = event.currentTarget.email;
+    const password = event.currentTarget.password;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    
+    const dataObj = {
+        Email: email.value,
+        Password: password.value
+    };
+    console.log(dataObj);
+    event.currentTarget.reset();
+});
